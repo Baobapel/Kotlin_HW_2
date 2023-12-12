6cbf7a2ae43a907846540ec2843f55f78ef5db15
Kirill <bra0610@yandex.ru>
2023-12-12 23:16:48 +0300

    CardCommission.kt fix

--- src/main/kotlin/CardCommission.kt
+++ src/main/kotlin/CardCommission.kt
@@ -1,7 +1,8 @@
 const val ERROR_TYPE = -1
 const val ERROR_LIMIT = -2
 fun main() {
- println(commissionCalculation("Mastercard", 10000.00, 1000.00 ))
+ println(commissionCalculation("Mastercard",
+     10000.00, 1000.00 ))
 }
 
 fun commissionCalculation(
@@ -16,15 +17,18 @@ fun commissionCalculation(
     val monthLimit = 600000.00
     val vkDayLimit = 15000.00
     val vkMonthLimit = 40000.00
-    if ((cardType == "Mastercard" || cardType == "Maestro") && amountTransfersInMonth <= 75000.00) {
+    if ((cardType == "Mastercard" || cardType == "Maestro")
+        && amountTransfersInMonth <= 75000.00) {
         commission = 0.0
-    } else if (currentDaySum >= dayLimit || amountTransfersInMonth >= monthLimit) {ERROR_LIMIT}
+    } else if (currentDaySum >= dayLimit ||
+        amountTransfersInMonth >= monthLimit) {ERROR_LIMIT}
     else {
         commission = currentDaySum * 0.006 + minCommissionOnMM
     }
 
          if (cardType == "Visa" || cardType == "Мир") {
-             if (currentDaySum > dayLimit || amountTransfersInMonth > monthLimit) {ERROR_LIMIT}
+             if (currentDaySum > dayLimit ||
+                 amountTransfersInMonth > monthLimit) {ERROR_LIMIT}
         } else if (currentDaySum * 0.0075 < minCommissionOnVM) {
              commission = minCommissionOnVM
          } else {
@@ -33,7 +37,8 @@ fun commissionCalculation(
 
         if (cardType == "VKPay") {
             commission = 0.0
-        } else if (currentDaySum > vkDayLimit || amountTransfersInMonth > vkMonthLimit) {ERROR_LIMIT}
+        } else if (currentDaySum > vkDayLimit ||
+            amountTransfersInMonth > vkMonthLimit) {ERROR_LIMIT}
      return commission
 }
 
